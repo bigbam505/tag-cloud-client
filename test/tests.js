2fc121c5b29992237c6050a4cca72ecbda2817b7
Brent Montague <bigbam505@gmail.com>
2013-03-28 20:16:55 -0400

    Add tests to tag-cloud-client

--- test/tests.js
+++ test/tests.js
@@ -1,3 +1,42 @@
-#!/usr/bin/env nodeo
-console.log('There are no tests at this time');
-process.exit(0)
+var client = require('../lib/tag-cloud-client.js'),
+    should = require('should'),
+    nock = require('nock');
+
+var testOptions = {
+  siteUrl: 'http://tag-cloud.com',
+  siteToken: 'apikey'
+}
+
+var Client = new client({url: testOptions.siteUrl, token: testOptions.siteToken})
+
+  describe('Client', function(){
+    describe('#version', function(){
+      it('should return a correct version number', function(){
+        Client.version.should.match(/^\d+\.\d+\.\d+$/);
+      })
+    })
+
+    describe('#GetRandomValue', function(){
+      nock(testOptions.siteUrl)
+      .get('/api/random_mapped_value?token=' + testOptions.siteToken + '&tag=random')
+      .reply(200, {
+        created_at: '2013-02-05T13:18:41Z',
+        id: 5,
+        tag_id: 3,
+        update_at: '2013-02-05T13:18:41Z',
+        value:'my-returned-value'
+      });
+
+    it('should return a random value for a tag', function(done){
+      Client.GetRandomValue('random', function(data){
+        data.should.include({value:'my-returned-value'});
+        done();
+      });
+    })
+    })
+  })
+
+
+
+
+
