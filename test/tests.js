a4791e7a4af36806c9411bdba75165a676ef3764
Brent Montague <bigbam505@gmail.com>
2013-05-02 00:11:44 -0400

    Add ability to add values to a tag

--- test/tests.js
+++ test/tests.js
@@ -16,15 +16,40 @@ var Client = new client({url: testOptions.siteUrl, token: testOptions.siteToken}
       })
     })
 
+    describe('#AddValue', function(){
+      nock(testOptions.siteUrl)
+      .post('/api/mapped_values', 'value=value&tag_name=tag-name&token='+ testOptions.siteToken)
+      .reply(200, {status:'OK'});
+
+    it('should return "OK" when a random value is added', function(done) {
+      Client.AddValue('value', 'tag-name', function(data) {
+        data.should.include({status:'OK'});
+        done();
+      })
+    });
+
+    nock(testOptions.siteUrl)
+      .post('/api/mapped_values', 'value=&tag_name=&token='+ testOptions.siteToken)
+      .reply(200, { error: "Something went terribly wrong" });
+
+    it('should return an error message when something goes wrong', function(done) {
+      Client.AddValue('','', function(data) {
+        data.should.not.include({status:'OK'});
+        data.should.include({error: "Something went terribly wrong"});
+        done();
+      })
+    });
+    })
+
     describe('#GetRandomValue', function(){
       nock(testOptions.siteUrl)
       .get('/api/random_mapped_value?token=' + testOptions.siteToken + '&tag=random')
       .reply(200, {
         created_at: '2013-02-05T13:18:41Z',
-        id: 5,
-        tag_id: 3,
-        update_at: '2013-02-05T13:18:41Z',
-        value:'my-returned-value'
+      id: 5,
+      tag_id: 3,
+      update_at: '2013-02-05T13:18:41Z',
+      value:'my-returned-value'
       });
 
     it('should return a random value for a tag', function(done){
